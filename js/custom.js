243d086d90719a2c1396bf3665e2e9609f3e784e
Humberto Perez <ferdinero121312@gmail.com>
2015-08-03 13:16:11 -0700

    add  cumstom file

---
+++ js/custom.js
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+	<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+	<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+	<!-- ////////////////////////////////////////////////
+		//// LINK TO YOUR CUSTOM CSS FILES HERE
+		//// <link type="text/css" href="css/style.css" rel="stylesheet"/>
+		///////////////////////////////////////////////////// -->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<title>Bootstrap 101 Template (DDC Version!)</title>
+</head>
+<body>
+<div class="container">
+	<h1>Hello, world! I'm a Bootstrap page! :D</h1>
+/**
+ * change background color
+ **/
+
+	function changeBackground (color){
+	document.body.style.background=color;
+	<BODY on load="changeBackground('red')"
+}
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac tristique risus. Nam rhoncus lacinia eros et tristique. Curabitur faucibus, quam sit amet placerat hendrerit, eros mi pretium metus, et posuere lectus diam mollis nisi. Curabitur pretium mollis cursus. Etiam quis lobortis massa, et ornare mi. Nunc vitae magna ipsum. Donec vulputate magna non laoreet accumsan. Sed sit amet tellus ac risus ultrices ullamcorper non eget purus. Etiam at pellentesque velit. Phasellus sagittis quam in erat fringilla posuere. Donec luctus, nisl at gravida finibus, sem diam pharetra erat, sed mattis enim urna vel elit. Curabitur accumsan dolor at est egestas finibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+	Nullam mattis in purus nec posuere. Quisque semper tellus in bibendum volutpat. Aliquam ut eros porta, placerat sem non, facilisis lorem. Phasellus vitae viverra nisi. Quisque feugiat, dolor quis blandit pharetra, velit dolor ultrices metus, a finibus metus neque eget enim. Proin venenatis metus vitae interdum accumsan. Morbi at arcu tortor. Curabitur sapien justo, lobortis nec eleifend id, consequat a turpis.
+
+	Aenean magna erat, sollicitudin eu interdum sed, volutpat ut diam. Aliquam ullamcorper commodo mauris, quis ornare tellus mattis ut. Aenean ut nibh in purus dignissim lacinia vel id nunc. Nulla facilisi. Quisque et consectetur arcu. Sed auctor tincidunt nisl non iaculis. Fusce suscipit pellentesque odio non dictum. Mauris ullamcorper magna euismod leo euismod, et egestas dui rutrum. Suspendisse nulla diam, tincidunt bibendum diam nec, dignissim condimentum magna. Aliquam erat volutpat. Vestibulum varius fringilla tortor, ac pulvinar velit varius a.
+
+	Aenean tempor sodales nulla vel scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus, ipsum ut fringilla suscipit, eros tortor laoreet neque, quis bibendum mi velit id eros. Integer sit amet lacinia nunc. Aenean eu purus sollicitudin, hendrerit enim ac, vulputate mauris. Nam in fermentum neque, varius condimentum tellus. Integer hendrerit luctus lorem ut efficitur. Nam convallis pretium justo, a dictum dui pretium et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac libero nulla. Nullam varius varius lectus non mollis. Praesent molestie tristique nisl non lacinia. Phasellus nec sem et risus volutpat luctus. Etiam eget diam tincidunt, lacinia lorem sollicitudin, ultrices velit. Praesent nec malesuada metus, sit amet feugiat mi.
+
+	Mauris ex risus, fermentum id dictum quis, scelerisque ut orci. Maecenas in auctor felis, sed ullamcorper massa. Donec id pharetra odio, sit amet cursus nisi. Donec dolor lacus, molestie at vehicula vestibulum, laoreet ut augue. Aliquam accumsan fermentum luctus. Quisque ante leo, auctor sed mi sed, imperdiet tincidunt dolor. In lacinia ligula ac eros suscipit condimentum. </p>Duis malesuada sem ac maximus facilisis. Curabitur diam ligula, accumsan vitae ex ut, auctor lobortis erat. Sed facilisis tortor vitae consectetur efficitur.
+</div>
+</body>
+</html>
