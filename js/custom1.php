3634a2a0d6cfd5611a362636c748911506b00370
Humberto Perez <ferdinero121312@gmail.com>
2015-08-03 14:19:41 -0700

    add  cumstom file

---
+++ js/custom1.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<h1 id="myHeader">estos son los versos de los Laureles!</h1>
+<button onclick="displayResult()">Change text</button>
+
+<script>
+function displayResult() {
+	document.getElementById("myHeader").innerHTML = " Estoy aprendiendo muchas cosas!";
+}
+</script>
+
+</body>
+</html>
+
